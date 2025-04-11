c726de59350d98dbbb425d1722b4685529345a3f
Weilong Chen <weilong.chen@tum.de>
2025-04-11 17:21:47 +0200

    reference

---
+++ examples/jax_md_benchmark/water/simulate.py
@@ -0,0 +1,195 @@
+import os
+import sys
+
+import argparse
+
+if len(sys.argv) > 1:
+    os.environ["CUDA_VISIBLE_DEVICES"] = sys.argv[1]
+
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1" # set your cuda device here instead od passing as argument
+
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.95"
+
+import jax
+# jax.config.update("jax_debug_nans", True)
+
+import jax
+from chemtrain.deploy import exporter, graphs as export_graphs
+
+from jax_md import partition, space, simulate
+from jax_md_mod.model import neural_networks
+from jax import numpy as jnp, tree_util
+import numpy as onp
+
+from collections import OrderedDict
+
+from chemtrain.data import graphs
+from chemtrain import trainers
+
+from chemutils.datasets import water
+
+import train_utils
+
+from ase.io.lammpsdata import read_lammps_data
+import pickle
+
+
+
+def get_default_config():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("device", type=str, default="-1")
+    parser.add_argument("tag", type=str, default=None)
+    parser.add_argument("--epochs", type=int, default=150)
+    parser.add_argument("--batch", type=int, default=128) # original 64
+    args = parser.parse_args()
+
+    print(f"Run on device {args.device}")
+    return OrderedDict(
+        tag=args.tag,
+        model=OrderedDict(
+            r_cutoff=0.50, # dimenet train 0.25
+            edge_multiplier=1.15,
+            # type="Allegro",
+            # model_kwargs=OrderedDict(
+            #     hidden_irreps="32x0e + 16x1e + 16x1o + 8x2e + 8x2o",
+            #     max_ell=3, # original 2
+            #     num_layers=2, # original 1
+            #     mlp_n_hidden=64,
+            #     embed_n_hidden=(8, 16, 32),
+            # ),
+            type="MACE",
+            model_kwargs=OrderedDict(
+                hidden_irreps="32x0e + 32x1o",
+                embed_dim=64,
+                readout_mlp_irreps="16x0e",
+                max_ell=2,
+                num_interactions=2,
+                correlation=3,
+            ),
+            # type="DimeNetPP",
+            # model_kwargs=OrderedDict(
+            #     embed_size=64, # origianl 128
+            #     n_interaction_blocks=2, # original 3
+            #     out_embed_size=192, # original 192
+            #     num_rbf=6,
+            #     num_sbf=7,
+            #     num_residual_before_skip=1,
+            #     num_residual_after_skip=2,
+            #     num_dense_out=3,
+            # ),
+            # type="PaiNN",
+            # model_kwargs=OrderedDict(
+            #     # hidden_size=128,
+            #     hidden_size=192,
+            #     n_layers=4,
+            # ),
+        ),
+        optimizer=OrderedDict(
+            init_lr=1e-4, # original 1e-3
+            # init_lr=1e-2,
+            # lr_decay=5e-2,
+            lr_decay=1e-05,
+            epochs=args.epochs,
+            batch=args.batch,
+            cache=25,
+            # weight_decay=-1e-2,
+            type="ADAM",
+            optimizer_kwargs=OrderedDict(
+                b1=0.9,
+                b2=0.99,
+                eps=1e-8
+            )
+        ),
+        gammas=OrderedDict(
+            U=1e-6,
+            F=1e-2,
+        ),
+    )
+
+def main():
+
+    config = get_default_config()
+    out_dir = train_utils.create_out_dir(config)
+
+    dataset = water.get_dataset("/home/weilong/workspace/chemsim-lammps/datasets")
+    dataset = water.get_random_subset(dataset, 1.0, seed=0)
+    displacement_fn, _ = space.periodic_general(box=dataset['training']['box'][0], fractional_coordinates=True)
+    if config["model"]["type"] == "DimeNetPP":
+        nbrs_format = partition.Dense
+        nbrs_init, (max_neighbors, max_edges, avg_num_neighbors, max_triplets) = graphs.allocate_neighborlist(
+            dataset["training"], displacement_fn, None, config["model"]["r_cutoff"], box_key="box", mask_key=None,
+            format=nbrs_format, count_triplets=True
+        )
+    else:
+        nbrs_init, (max_neighbors, max_edges, avg_num_neighbors) = graphs.allocate_neighborlist(
+            dataset["training"], displacement_fn, None, config["model"]["r_cutoff"], box_key="box", mask_key=None,
+            format=partition.Sparse
+        )
+
+    print(f"Neighbors: {nbrs_init}")
+    print(f"Max neighbors: {max_neighbors}, max edges: {max_edges}")
+
+    # Positive species not required -> check again
+    if config["model"]["type"] == "DimeNetPP":
+        energy_fn_template, init_params = train_utils.define_model(
+            config, dataset, nbrs_init, max_edges, per_particle=False, # per_particle=False for trainig
+            avg_num_neighbors=avg_num_neighbors, positive_species=False,
+            displacement_fn=displacement_fn, max_triplets=max_triplets
+        ) 
+    else:
+        energy_fn_template, init_params = train_utils.define_model(
+            config, dataset, nbrs_init, max_edges, per_particle=False, # per_particle=False for trainig
+            avg_num_neighbors=avg_num_neighbors, positive_species=False,
+            displacement_fn=displacement_fn
+        )      
+    energy_params = onp.load(
+        "best_params.pkl",
+        allow_pickle=True,
+    )
+
+    energy_params = tree_util.tree_map(
+        jnp.asarray, energy_params
+    )
+    energy_fn = energy_fn_template(energy_params)
+
+    atoms = read_lammps_data("final_config_filtered_13056_atoms.data", style='atomic')
+    
+    positions = atoms.get_positions() / 10 # convert to nm
+    box = atoms.cell
+    species = atoms.get_atomic_numbers()
+    box_lengths = jnp.array([box[0, 0], box[1, 1], box[2, 2]]) / 10
+    R = jnp.array(positions)
+    species = jnp.array(species) - 1
+
+    masses = jnp.zeros_like(species, dtype=jnp.float32)
+    masses = jnp.where(species == 0, 15.9994, masses)
+    masses = jnp.where(species == 1, 1.008, masses)
+
+
+    init_sample = {
+        "R": R,
+        "box": box_lengths,
+        "species": species,
+        "mass": masses,
+
+    } 
+    fractional = True
+    displacement_fn, shift_fn = space.periodic_general(
+    box_lengths, fractional_coordinates=fractional)
+
+    neighbor_fn = partition.neighbor_list(
+        displacement_fn, box_lengths, config["model"]["r_cutoff"], dr_threshold=0.01)
+    nbrs_init = neighbor_fn.allocate(init_sample["R"])
+
+    reference_state, traj_generator = train_utils.init_simulator(
+        init_sample, energy_fn, nbrs_init
+    )
+    traj_state = traj_generator(None, reference_state)
+    trajectory = traj_state.trajectory.position
+
+
+if __name__ == "__main__":
+    main()
+
+
+
